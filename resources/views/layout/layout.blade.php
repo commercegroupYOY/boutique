b6dab23ff6e45603e606d6d5fa5d343422b9adca
olivier <olivier.himblot@le-campus-numerique.fr>
2023-07-05 13:26:59 +0200

    first commit

---
+++ resources/views/layout/layout.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite(['resources/css/app.css'])
+    @vite(['resources/js/app.js'])
+    @vite(['resources/js/script.js'])
+    <script
+  type="text/javascript"
+  src="../node_modules/tw-elements/dist/js/tw-elements.umd.min.js"></script>
+    <title>RetroRogilo</title>
+</head>
+<body>
+    @include('layout.header')
+
+
+
+
+    @include('layout.footer')
+
+</body>
+</html>
