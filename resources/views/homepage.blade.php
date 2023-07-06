4a5d80c1c6326b480fe2e5d51827fe7a52d5dd47
olivier <olivier.himblot@le-campus-numerique.fr>
2023-07-06 09:44:32 +0200

    fin accueil

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,6 +5,9 @@ vous aimez de retro gaming ?<br>
 découvrez une large sélection de console et de jeux vidéos !<br>
 pensez aussi à regarder nos vidéos sur YouTube et à nous suivre sur les réseaux sociaux !<br>
 </div>
+
 @include('layout.carouselconsole')
+
+@include('layout.catalog_choice')
 
 @include('layout.footer')
