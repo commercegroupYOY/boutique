0bd66670021b1741c2a2564f2150382a704d871a
Yan SEGUIN <yan.seguin@Digital-Grenoble.local>
2023-07-06 14:59:45 +0200

    bug fix

--- resources/views/layout/_home/_catalog_choice.blade.php
+++ resources/views/layout/_home/_catalog_choice.blade.php
@@ -2,15 +2,15 @@
     <img src="{{asset ('img/console.png')}}" class="mx-auto" alt="en-tête">
 </div>
 <div class="flex justify-center mx-auto p-10">
-<button class="bg-transparent hover:bg-blue-600 text-black font-semibold hover:text-black py-2 px-4 border border-dark hover:border-transparent rounded">
-    CONSOLE
-  </button>
+    <a href="/Liste des produits" class="bg-transparent hover:bg-blue-600 text-black font-semibold hover:text-black py-2 px-4 border border-dark hover:border-transparent rounded">
+        JEUX
+    </a>
 </div>
 <div class="mx-auto">
     <img src="{{asset ('img/Jeux.png')}}" class="mx-auto" alt="en-tête">
 </div>
 <div class="flex justify-center mx-auto p-10">
-<button class="bg-transparent hover:bg-blue-600 text-black font-semibold hover:text-black py-2 px-4 border border-dark hover:border-transparent rounded">
+<a href="/Liste des produits" class="bg-transparent hover:bg-blue-600 text-black font-semibold hover:text-black py-2 px-4 border border-dark hover:border-transparent rounded">
     JEUX
-  </button>
+</a>
 </div>
