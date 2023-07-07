e2eab8f21d5cf5f76b5db18abfa5c42928e41db9
HIMBLOT Olivier <olivier.himblot@le-campus-numerique.fr>
2023-07-07 16:09:54 +0200

    correction mineur

--- resources/views/layout/_product/_cardProduct.blade.php
+++ resources/views/layout/_product/_cardProduct.blade.php
@@ -1,16 +1,16 @@
         @foreach ($product as $products)
 <div class=" w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
     <form action="{{ url('Details_produit/'.$products->id) }}" method="">
- 
-        <input type="hidden" name="id" for="id" value="<?=$products->id?>">
-        <button type="submit" class="btn btn-primary"> <img class="p-8 rounded-t-lg" src="<?= $products->picture_url_yoy ?>" alt="product image" /></button>
+
+        <input type="hidden" name="id" for="id" value="{{$products->id}}">
+        <button type="submit" class="btn btn-primary"> <img class="p-8 rounded-t-lg" src="{{ $products->picture_url_yoy }}" alt="product image" /></button>
     </form>
     <div class=" px-5 pb-5">
         <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-               <?= $products->name  ?></h5>
+               {{ $products->name  }}</h5>
 
         <div class=" items-center justify-between">
-            <span class=" text-3xl font-bold text-gray-900 dark:text-white"><?= $products->price  ?></span>
+            <span class=" text-3xl font-bold text-gray-900 dark:text-white">{{ $products->price  }}</span>
 
             <a href="#"
                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add
--- resources/views/productDetail.blade.php
+++ resources/views/productDetail.blade.php
@@ -2,30 +2,30 @@
 
 @foreach ($product as $products)
 @section('title')
-<?=$products->name ?>
+{{$products->name }}
 @endsection
 
 @section('content')
 @dump($products->picture_url_yoy)
     <div class="py-8">
-        <h1 class="text-2xl text-center ps-10"> <?=$products->name ?> </h1>
+        <h1 class="text-2xl text-center ps-10"> {{$products->name }} </h1>
     </div>
     <section class="text-gray-700 body-font overflow-hidden bg-white">
         <div class="container px-5 py-24 mx-auto">
             <div class="lg:w-4/5 mx-auto flex flex-wrap">
                 <img alt="ecommerce" class="lg:w-1/2 w-full object-cover object-center rounded border border-gray-200" src=" /{{$products->picture_url_yoy}} ">
                 <div class="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
-                    <h2 class="text-sm title-font text-gray-500 tracking-widest"><?=$products->category?></h2>
-                    <h1 class="text-gray-900 text-3xl title-font font-medium mb-1"><?=$products->name ?></h1>
+                    <h2 class="text-sm title-font text-gray-500 tracking-widest">{{$products->category}}</h2>
+                    <h1 class="text-gray-900 text-3xl title-font font-medium mb-1">{{$products->name }}</h1>
 
                     <div class="flex mb-4">
                     </div>
 
-                    <p class="leading-relaxed"><?=$products->description ?></p>
+                    <p class="leading-relaxed">{{$products->description }}</p>
 
                 </div>
                 <div class="mt-5 container flex">
-                    <span class=" flex-1 title-font font-medium text-2xl text-gray-900"><?=$products->price ?>
+                    <span class=" flex-1 title-font font-medium text-2xl text-gray-900">{{$products->price }}
                         </span>
                     <button class="flex-1 items-center justify-between">
                         <a href="#"
