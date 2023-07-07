c08f2b9638e2ba17e1c917c1aee16582ef7ecd1e
HIMBLOT Olivier <olivier.himblot@le-campus-numerique.fr>
2023-07-07 16:56:10 +0200

    maj bdd et ajout d'une nouvelle categorie

--- resources/views/layout/_home/_catalog_choice.blade.php
+++ resources/views/layout/_home/_catalog_choice.blade.php
@@ -2,17 +2,26 @@
     <img src="{{ asset('img/console.png') }}" class="mx-auto" alt="en-tête">
 </div>
 <div class="flex justify-center mx-auto p-10">
-    <a href="/Liste des produits"
+    <a href="/Liste_des_produits"
         class="bg-transparent w-full text-center hover:bg-blue-600 text-black font-semibold hover:text-black py-2 px-4 border border-dark hover:border-transparent rounded">
-        JEUX
+        CONSOLE
     </a>
 </div>
 <div class="mx-auto">
     <img src="{{ asset('img/Jeux.png') }}" class="mx-auto" alt="en-tête">
 </div>
 <div class="flex justify-center mx-auto p-10">
-    <a href="/Liste des produits"
+    <a href="/Liste_des_produits"
         class="bg-transparent w-full text-center hover:bg-blue-600 text-black font-semibold hover:text-black py-2 px-4 border border-dark hover:border-transparent rounded">
         JEUX
+    </a>
+</div>
+<div class="mx-auto">
+    <img src="{{ asset('img/merch.jfif') }}" class="mx-auto" alt="en-tête">
+</div>
+<div class="flex justify-center mx-auto p-10">
+    <a href="/Liste_des_produits"
+        class="bg-transparent w-full text-center hover:bg-blue-600 text-black font-semibold hover:text-black py-2 px-4 border border-dark hover:border-transparent rounded">
+        Merchandising
     </a>
 </div>
