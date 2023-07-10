fc1b7cdf094beca7108999ab2bbf188e02e6ba55
HIMBLOT Olivier <olivier.himblot@le-campus-numerique.fr>
2023-07-10 16:55:46 +0200

    fin journée

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -81,7 +81,7 @@
                                 <!-- Second dropdown menu items -->
                                 <li>
                                     <a class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400"
-                                        href="#" data-te-dropdown-item-ref>Profil</a>
+                                        href="/dashboard" data-te-dropdown-item-ref>Profil</a>
                                 </li>
 
                             </ul>
