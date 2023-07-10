1359e01c8f7d823458ed18823696d0c11e0a70dd
HIMBLOT Olivier <olivier.himblot@le-campus-numerique.fr>
2023-07-10 16:51:53 +0200

    correction tw elements

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,13 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+import {
+    Collapse,
+    Dropdown,
+    initTE,
+  } from "tw-elements";
+
+  initTE({ Collapse, Dropdown });
+
+  
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ export default {
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/tw-elements/dist/js/**/*.js"
     ],
 
     theme: {
@@ -17,5 +21,6 @@ export default {
         },
     },
 
-    plugins: [forms],
+    plugins: [require("tw-elements/dist/plugin.cjs")]
 };
+
